b45100818c08886854893e208f00218d948302f0
Nicholas Ionata <n.ionata129@gmail.com>
2017-01-21 12:26:47 -0500

    Added custom color to nabber

--- Converge/AppDelegate.swift
+++ Converge/AppDelegate.swift
@@ -16,8 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
 		FIRApp.configure()
+		UINavigationBar.appearance().barTintColor = UIColor.blue
+		UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
 		return true
 	}
 
