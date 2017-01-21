41fc120f8ead88228569129d2b26b426626ab7c8
Nicholas Ionata <n.ionata129@gmail.com>
2017-01-21 14:37:54 -0500

    Added home page for events and routing to specific tab page

--- Converge/HomeTableViewController.swift
+++ Converge/HomeTableViewController.swift
@@ -7,89 +7,84 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class HomeTableViewController: UITableViewController {
+	
+	@IBOutlet var table: UITableView!
+	var ref = FIRDatabase.database().reference()
+	var events: [String] = []
+	var currentSegue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+		loadEvents()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+	override func viewDidAppear(_ animated: Bool) {
+		FIRAuth.auth()?.addStateDidChangeListener { (auth, user) in
+			if user == nil {
+				self.performSegue(withIdentifier: "toSignIn", sender: self)
+			}
+		}
+		
+		self.table.reloadData()
+	}
+	
+	func loadEvents() {
+		let usersRef = ref.child("users").child((FIRAuth.auth()?.currentUser?.uid)!).child("events")
+		usersRef.observeSingleEvent(of: .value, with: { (snapshot) in
+			if let dict = snapshot.value as? [String: AnyObject] {
+				for key in dict {
+					self.events.append(key.key)
+				}
+			}
+			
+			if(self.events.count == 0) {
+				let alertController = UIAlertController(title: "Welcome!", message: "You are not joined or created an event. Start by tapping the top right", preferredStyle: .alert)
+				let newAction = UIAlertAction(title: "Get Started", style: .default, handler: { (alert) in
+					self.performSegue(withIdentifier: "toCreateAccount", sender: self)
+				})
+				let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+				alertController.addAction(defaultAction)
+				alertController.addAction(newAction)
+				self.present(alertController, animated: true, completion: nil)
+			}
+			
+			self.table.reloadData()
+		})
+	}
+	
+	@IBAction func onLogOut(_ sender: Any) {
+		try! FIRAuth.auth()?.signOut()
+	}
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return events.count
     }
 
-    /*
+	
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		cell.textLabel?.text = events[indexPath.row]
         return cell
     }
-    */
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		self.currentSegue = indexPath.row
+		self.performSegue(withIdentifier: "toEventTabs", sender: self)
+	}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+		if(segue.identifier == "toEventTabs") {
+			let newView = segue.destination as! UITabBarController
+			let memView = newView.viewControllers?.first as! EventFormViewController
+			memView.event = events[currentSegue]
+			currentSegue = 0
+		}
     }
-    */
+
 
 }
