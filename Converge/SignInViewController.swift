88e29b93458f10da2357db894f92ddb2326befb7
Nicholas Ionata <n.ionata129@gmail.com>
2017-01-21 08:55:42 -0500

    Initial commit

---
+++ Converge/SignInViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Converge
+//
+//  Created by Nicholas Ionata on 1/21/17.
+//  Copyright © 2017 Nicholas Ionata. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignInViewController: UIViewController, UITextFieldDelegate {
+
+	@IBOutlet weak var newEmail: UITextField!
+	@IBOutlet weak var newPassword: UITextField!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		view.endEditing(true)
+	}
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		if(textField == self.newEmail) {
+			self.newPassword.becomeFirstResponder()
+		} else if(textField == self.newPassword) {
+			onSignIn(self)
+		}
+		return false
+	}
+
+	@IBAction func onSignIn(_ sender: Any) {
+		if(newEmail.text != "" || newPassword.text != "") {
+			FIRAuth.auth()?.signIn(withEmail: self.newEmail.text!, password: self.newPassword.text!, completion: { (user, error) in
+				if(error == nil) {
+					self.newEmail.text = ""
+					self.newPassword.text = ""
+					self.newPassword.resignFirstResponder()
+					//Perform some segue
+				} else {
+					let alertController = UIAlertController(title: "Oops!", message: error?.localizedDescription, preferredStyle: .alert)
+					let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+					alertController.addAction(defaultAction)
+					self.present(alertController, animated: true, completion: nil)
+				}
+			})
+		} else {
+			let alertController = UIAlertController(title: "Oops!", message: "Enter an email and password", preferredStyle: .alert)
+			let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+			alertController.addAction(defaultAction)
+			self.present(alertController, animated: true, completion: nil)
+		}
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Converge/SignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpViewController.swift
+//  Converge
+//
+//  Created by Nicholas Ionata on 1/21/17.
+//  Copyright © 2017 Nicholas Ionata. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
