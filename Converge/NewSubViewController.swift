ce78c5105a0dd8e28d758da2e6a4f1f8331a0b44
Nicholas Ionata <n.ionata129@gmail.com>
2017-01-21 20:34:58 -0500

    Added view for new idea or freelancer

---
+++ Converge/NewSubViewController.swift
@@ -0,0 +1,65 @@
+//
+//  NewSubViewController.swift
+//  Converge
+//
+//  Created by Nicholas Ionata on 1/21/17.
+//  Copyright © 2017 Nicholas Ionata. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
+class NewSubViewController: UIViewController {
+
+	@IBOutlet weak var newIdea: UITextField!
+	var ref = FIRDatabase.database().reference()
+	var event: String = ""
+	var ideaOrPerson: Int = 0
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		if(ideaOrPerson == 0) {
+			newIdea.placeholder = "Write your idea here"
+		} else if(ideaOrPerson == 1) {
+			newIdea.placeholder = "Write your specialty here"
+		}
+    }
+	
+	@IBAction func onAddIdea(_ sender: Any) {
+		if(newIdea.text != "") {
+			var subType: String = ""
+			let user = FIRAuth.auth()?.currentUser
+			
+			if(ideaOrPerson == 0) {
+				subType = "idea"
+			} else if(ideaOrPerson == 1) {
+				subType = "freelancer"
+			}
+			
+			let childRef = ref.child("subs").child(event).child(subType).childByAutoId()
+			let ideaCode = childRef.key
+			childRef.child("name").setValue(user?.displayName)
+			childRef.child("idea").setValue(newIdea.text)
+			childRef.child("creator").setValue(user?.uid)
+			
+			let userRef = ref.child("users").child((user?.uid)!).child("events").child(event)
+			userRef.child(ideaCode).setValue("owner")
+			
+			let eventRef = ref.child("events").child(event).child(ideaCode)
+			eventRef.child((user?.uid)!).child("name").setValue(user?.displayName)
+			
+			navigationController?.popToRootViewController(animated: true)
+		} else {
+			let alertController = UIAlertController(title: "Oops!", message: "Enter an idea.", preferredStyle: .alert)
+			let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+			alertController.addAction(defaultAction)
+			self.present(alertController, animated: true, completion: nil)
+		}
+	}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
