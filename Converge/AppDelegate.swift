4933e2245fac5ca1f5d7225b44fbdf5114c106d6
Nicholas Ionata <n.ionata129@gmail.com>
2017-01-21 20:33:27 -0500

    Added navcontroller color

--- Converge/AppDelegate.swift
+++ Converge/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		FIRApp.configure()
 		UINavigationBar.appearance().barTintColor = UIColor.blue
+		UINavigationBar.appearance().tintColor = UIColor.white
 		UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
 		return true
 	}
